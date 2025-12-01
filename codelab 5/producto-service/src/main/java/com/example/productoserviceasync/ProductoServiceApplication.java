6bb25a9401e1698352bb1195c5280ecd99dbc7b8
Andres Felipe Rojas <107456253+Bakudi@users.noreply.github.com>
2025-12-01 16:48:40 -0500

    subidos y ordenados

---
+++ codelab 5/producto-service/src/main/java/com/example/productoserviceasync/ProductoServiceApplication.java
@@ -0,0 +1,11 @@
+package main.java.com.example.productoserviceasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductoServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductoServiceApplication.class, args);
+    }
+}
